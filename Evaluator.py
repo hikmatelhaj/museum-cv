06b4068dbd1bcae28015c715b47e2397cfd387cc
Jonas Simoen <jonas.simoen@ugent.be>
2023-05-12 16:16:08 +0200

    refactor

--- Evaluator.py
+++ Evaluator.py
@@ -108,18 +108,24 @@ class Evaluator:
         log["Bottom-left"] = log["Bottom-left"].apply(ast.literal_eval)
         log["Bottom-right"] = log["Bottom-right"].apply(ast.literal_eval)
 
+        # total statistics
         total_fp = 0
         total_fn = 0
         total_iou = 0
         total_imgs = 0
 
+        # loop over all images
         for img_path in glob.glob(f"{images_folder_path}/*/*.jpg"):
+            # extract room & image name out of path
             _, zaal, img_name = img_path.split("\\")
             img_name = img_name.strip(".jpg")
 
             img_paintings_gt = []
+
+            # get all groundtruth boxes out of the log file
             img_log_entries = log[(log['Room'] == zaal) & (log['Photo'] == img_name)]
 
+            # create list of all groundtruth boxes
             for idx, img_log_entry in img_log_entries.iterrows():
                 img_paintings_gt.append(
                     np.array([img_log_entry["Top-left"], img_log_entry["Top-right"],
@@ -127,9 +133,13 @@ class Evaluator:
                 )
 
             image = cv.imread(img_path)
+
+            # there are no paintings in this image (no groundtruth boxes) ==> skip
             if len(img_paintings_gt) == 0:
                 print(f"<!> {img_path} does not contain any painting")
                 continue
+
+            # evaluation of the paintings in this image
             fn, fp, avg_iou = self.evaluate_image(image, img_paintings_gt)
 
             total_imgs += 1
@@ -137,6 +147,7 @@ class Evaluator:
             total_fn += fn
             total_fp += fp
 
+            # some logging
             if verbose == 1:
                 print(f"{img_path}:\t\t{fn} missed\t\t{fp} non-paintings extracted\t\t{avg_iou} IOU")
             if verbose == 2:
@@ -145,6 +156,7 @@ class Evaluator:
                 cv.waitKey(0)
                 cv.destroyAllWindows()
 
+        # final report
         print(f"Report of all images ({total_imgs}):\n\tMissed paintings:\t {total_fn}\n\tExtracted non-paintings: {total_fp}\n\tAverage IoU:\t\t {total_iou/total_imgs}")
                 
 
--- Extractor.py
+++ Extractor.py
@@ -7,72 +7,90 @@ import ast
 from shapely.geometry import Polygon
 import os
 
+
 class Extractor:
     """
     Extracts paintings out of images like a magician
     """
 
-    def find_painting(self, image):
+    def find_painting(self, image: np.ndarray) -> list:
         """
         Extract painting polygons out of the image
+        :return: list of polygons (extracted paintings)
         """
         gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
         ret, th = cv.threshold(gray, 0, 255, cv.THRESH_BINARY+cv.THRESH_OTSU)
-        blur = cv.GaussianBlur(th,(5,5),0)
+        blur = cv.GaussianBlur(th, (5, 5), 0)
         edges = cv.Canny(blur, 50, 120)
         dilated = cv.dilate(edges, np.ones((5, 5), np.uint8), iterations=3)
-    
-        contours = cv.findContours(dilated, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)[0]
+
+        contours = cv.findContours(
+            dilated, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)[0]
         contours = sorted(contours, key=cv.contourArea, reverse=True)[:10]
-        
+
         cut_list = []
         for c in contours:
             polygon = cv.approxPolyDP(c, 0.05*cv.arcLength(c, True), True)
             if len(polygon) == 4:
+                # only rectangular shapes
                 polygon = polygon.reshape(4, 2)
+                # too small polygons are filtered
                 if Polygon(polygon).area > 15000:
-                    image = cv.polylines(image, [polygon], True, (0, 0, 255), 10)
+                    image = cv.polylines(
+                        image, [polygon], True, (0, 0, 255), 10)
                     cut_list.append(polygon)
         return cut_list
-    
-    def crop_to_painting(self, image, polygon):
+
+    def crop_to_painting(self, image: np.ndarray, polygon: np.ndarray) -> np.ndarray:
         """
         Transform image based on the extracted painting polygons of the image
         """
-        maxW = max(polygon[:,0]) - min(polygon[:,0])
-        maxH = max(polygon[:,1]) - min(polygon[:,1])
+        maxW = max(polygon[:, 0]) - min(polygon[:, 0])
+        maxH = max(polygon[:, 1]) - min(polygon[:, 1])
         bounding_box = np.array([
-            [0, 0],
-            [0, maxH],
-            [maxW, maxH],
-            [maxW, 0]], dtype="float32")
-        transform = cv.getPerspectiveTransform(np.float32(polygon), bounding_box)
+            [0, 0],         # top left
+            [0, maxH],      # bottom left
+            [maxW, maxH],   # bottom right
+            [maxW, 0]],     # bottom right
+            dtype="float32")
+
+        # transform image
+        transform = cv.getPerspectiveTransform(
+            np.float32(polygon), bounding_box)
         result = cv.warpPerspective(image, transform, (maxW, maxH))
         return result
-    
-    # process single image and transform to cropped image
-    def extract_and_crop_image(self, image, drawPolygons=True):
+
+    def extract_and_crop_image(self, image: np.ndarray, drawPolygons: bool = True):
         """
         Process a single images, extracts all paintings 
         and creates new images, each with the cropped painting
+        :return: cropped images of paintings
         """
+        # extract paintings
         polygons = self.find_painting(image)
 
         result_imgs = []
 
         for polygon in polygons:
+            # draw if necessary
             if drawPolygons:
                 img = cv.polylines(img, [polygon], True, (0, 0, 255), 10)
-            
-            maxW = max(polygon[:,0]) - min(polygon[:,0])
-            maxH = max(polygon[:,1]) - min(polygon[:,1])
+
+            # calculate new coordinatees
+            maxW = max(polygon[:, 0]) - min(polygon[:, 0])
+            maxH = max(polygon[:, 1]) - min(polygon[:, 1])
             bounding_box = np.array([
                 [0, 0],
                 [0, maxH],
                 [maxW, maxH],
                 [maxW, 0]], dtype="float32")
-            transform = cv.getPerspectiveTransform(np.float32(polygon), bounding_box)
+            
+            # transformation of image
+            transform = cv.getPerspectiveTransform(
+                np.float32(polygon), bounding_box)
             result = cv.warpPerspective(img, transform, (maxW, maxH))
+
+            # add new painting image to result list
             result_imgs.append(result)
-        
+
         return result_imgs
