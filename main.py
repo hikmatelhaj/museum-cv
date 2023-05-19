0ecefd2549a42729780f6b17fefdc90773460b91
Jonas Simoen <jonas.simoen@ugent.be>
2023-05-19 16:56:58 +0200

    update main: checking test pictures

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 from Evaluator import Evaluator
+import glob
+import cv2 as cv
+
+from Extractor import Extractor
 
 # TODO
 
-evaluator = Evaluator()
-evaluator.evaluate_all_images("./data/Database2", "./data/Database_log.csv")
+# extractor = Extractor()
+# for img_path in glob.glob("./data/test_pictures_msk/*.jpg"):
+#     image = cv.imread(img_path)
+#     results = extractor.extract_and_crop_image(image, True)
+#     cv.imshow("Original", cv.resize(image, (int(image.shape[1]/5),int(image.shape[0]/5))))
+#     for idx, extracted_painting in enumerate(results):
+#         cv.imshow(f"Painting {idx}", cv.resize(extracted_painting, (int(extracted_painting.shape[1]/5),int(extracted_painting.shape[0]/5))))
+#     cv.waitKey(0)
+#     cv.destroyAllWindows()
+
+
+
+# evaluator = Evaluator()
+# evaluator.evaluate_all_images("./data/Database2", "./data/Database_log.csv")
