37cb8d733e1bbc14f35816794b9de6af20f4b1d1
Jonas Simoen <jonas.simoen@ugent.be>
2023-05-22 10:33:31 +0200

    add undistorting for gopro vids

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import glob
 import cv2 as cv
 
 from Extractor import Extractor
+from Rectifier import Rectifier
 
 # TODO
 
@@ -20,3 +21,28 @@ from Extractor import Extractor
 
 # evaluator = Evaluator()
 # evaluator.evaluate_all_images("./data/Database2", "./data/Database_log.csv")
+
+
+rectifier = Rectifier()
+rectifier.calibrate_by_video("telin.ugent.be/~dvhamme/computervisie_2022/videos/gopro/calibration_W.mp4")
+
+
+print("calibrated")
+cap = cv.VideoCapture("telin.ugent.be/~dvhamme/computervisie_2022/videos/gopro/MSK_15.mp4")
+fps = int(cap.get(cv.CAP_PROP_FPS))
+
+f = 0
+while cap.isOpened():
+    process = False
+    ret, frame = cap.read()
+    if not ret:
+        break
+    if f < fps:
+        f += 1
+    if f % fps == 0:
+        f = 1
+        rectified_img = rectifier.process_image(frame)
+        cv.imshow("Image", frame)
+        cv.imshow("Rectified", rectified_img)
+        cv.waitKey(0)
+        cv.destroyAllWindows()
